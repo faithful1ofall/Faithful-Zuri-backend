297297d870c3c720ff26fc34edd5f77037ad2edd
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-10 20:02:04 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const admin = require('firebase-admin');
 
-const serviceAccount = {
+/*const serviceAccount = {
   "type": process.env.FIREBASE_TYPE,
   "project_id": process.env.FIREBASE_PROJECT_ID,
   "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
@@ -12,7 +12,7 @@ const serviceAccount = {
   "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
   "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
   "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN,
-};
+};*/
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
