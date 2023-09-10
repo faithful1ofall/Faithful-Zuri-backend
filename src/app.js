189af0dc4f1aba69757284a1cf31939646473dac
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-10 19:45:38 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./test.json');
+const serviceAccount = {
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
