14405e30e4a223b0f72d4e47e8f46508f6c5a221
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-10 19:34:40 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,43 @@
 const express = require('express');
+const admin = require('firebase-admin');
 
+const serviceAccount = require('./test.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://faithful-ads.firebaseio.com',
+});
 
 const app = express();
+const db = admin.database();
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Zuri Faithfuls First backend');
+  res.send('Welcome to the Zuri Faithfuls First backend');
+});
+
+app.post('/api/persons', (req, res) => {
+  const { name, age } = req.body;
+
+  if (!name || typeof age !== 'number') {
+    return res.status(400).json({ error: 'Invalid data' });
+  }
+
+  const newPersonRef = db.ref('persons').push();
+  newPersonRef.set({ name, age }, (error) => {
+    if (error) {
+      res.status(500).json({ error: 'Error adding person' });
+    } else {
+      res.status(201).json({ message: 'Person added successfully' });
+    }
   });
+});
 
-
-
-app.get('/api', (req, res) => {
+app.get('/api1', (req, res) => {
   const slackName = req.query.slack_name;
   const track = req.query.track;
-  const currentDate = new Date;
+  const currentDate = new Date();
   const currentDay = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
   const utcTime = currentDate.toISOString().slice(0, -5) + 'Z';
 
